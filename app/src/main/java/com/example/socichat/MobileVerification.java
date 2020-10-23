ffb729781ebc3f7088a19f712352abdfbce389e1
AhmedQotb <50117886+AhmedQotbAhmed@users.noreply.github.com>
2020-10-23 15:40:41 +0200

    .

---
+++ app/src/main/java/com/example/socichat/MobileVerification.java
@@ -0,0 +1,14 @@
+package com.example.socichat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MobileVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mbile_verification);
+    }
+}
