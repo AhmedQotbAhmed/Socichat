5f8af2ea5e5e5d12d02cf630ddbac72a3b0773e4
AhmedQotb <50117886+AhmedQotbAhmed@users.noreply.github.com>
2020-10-26 01:28:12 +0200

    .

--- app/src/main/java/com/example/socichat/Fragment/ChatFragment.java
+++ app/src/main/java/com/example/socichat/Fragment/ChatFragment.java
@@ -1,22 +1,30 @@
 package com.example.socichat.Fragment;
 
+import android.app.SearchManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.socichat.Fragment.adapter.ChatAdapter;
 import com.example.socichat.R;
 
+import static android.content.Context.SEARCH_SERVICE;
 
 
 public class ChatFragment extends Fragment {
     RecyclerView recyclerView;
+    private SearchView searchView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -26,13 +34,38 @@ public class ChatFragment extends Fragment {
         recyclerView = view.findViewById(R.id.recyclerView_chat);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         ChatAdapter chatAdapter= new ChatAdapter();
-
         recyclerView.setAdapter(chatAdapter);
 
 
-
+        setHasOptionsMenu(true);
         return view;
 
 
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_search, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+        SearchManager searchManager = (SearchManager) getContext().getSystemService(SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.action_search)
+                .getActionView();
+        searchView.setBackgroundColor((0xFFF));
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getActivity().getComponentName()));
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        setHasOptionsMenu(true);
+        searchView.setQueryHint("Search Product");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
