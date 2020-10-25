2bdb60b20aee7fc25630e6a6f4254e285094a208
AhmedQotb <50117886+AhmedQotbAhmed@users.noreply.github.com>
2020-10-25 18:18:32 +0200

    .

--- app/src/main/java/com/example/socichat/MainActivity.java
+++ app/src/main/java/com/example/socichat/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.socichat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+
+        setSupportActionBar(toolbar);
+        Button btn=findViewById(R.id.btn_send_otp);
+        btn.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+            switch (v.getId()){
+                case R.id.btn_send_otp:
+                    Intent outIntent = new Intent(MainActivity.this, MobileVerification.class);
+                    startActivity(outIntent);
+            }
+
     }
 }
--- app/src/main/java/com/example/socichat/MobileVerification.java
+++ app/src/main/java/com/example/socichat/MobileVerification.java
@@ -1,14 +1,54 @@
 package com.example.socichat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MobileVerification extends AppCompatActivity {
+import com.alimuzaffar.lib.pin.PinEntryEditText;
+
+
+public class MobileVerification extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mbile_verification);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_verifi);
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+
+        final PinEntryEditText pinEntry = (PinEntryEditText) findViewById(R.id.edt_verific_code);
+        if (pinEntry != null) {
+            pinEntry.setOnPinEnteredListener(new PinEntryEditText.OnPinEnteredListener() {
+                @Override
+                public void onPinEntered(CharSequence str) {
+                    if (str.toString().equals("1234")) {
+                        Toast.makeText(MobileVerification.this, "SUCCESS", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MobileVerification.this, "FAIL", Toast.LENGTH_SHORT).show();
+                        pinEntry.setText(null);
+                    }
+                }
+            });
+        }
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
